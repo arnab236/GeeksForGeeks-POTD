11f553cc173db5a81ae1d41bdb7a207d4d03e276
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2023-05-14 15:00:43 +0530

    Create Maximum Subset sum.cpp

---
+++ Maximum Subset sum.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class IntArray
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    long long findMaxSubsetSum(int N, vector<int> &A) {
+        // code here
+        long long m[N+1][2];
+        m[0][0]=0;
+        m[0][1]=0;
+        for(int i=1;i<=N;i++)
+        {
+            m[i][0] = m[i-1][1];
+            m[i][1] = max(m[i-1][0] +A[i-1],m[i-1][1]+A[i-1]);
+            
+        }
+        return max(m[N][0],m[N][1]);
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        IntArray::input(A,N);
+        
+        Solution obj;
+        long long res = obj.findMaxSubsetSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
